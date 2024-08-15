c871bdcff0a87b88e549b200f358077c70ddce88
Merloj <40292375+Maciekxdabu@users.noreply.github.com>
2024-08-15 17:39:53 +0200

    Back to Menu button (on HUD)

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
     [SerializeField] private int staringScore = 5;
     [SerializeField] private int plantingCost = 1;
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private string mainMenuScene;
 
     private int score = 0;
 
@@ -44,6 +46,13 @@ public class HUD : MonoBehaviour
             UpdateHUD();
             return true;
         }
+    }
+
+    // ---------- public methods (for buttons)
+
+    public void OnBackToMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
     }
 
     // ---------- private methods
